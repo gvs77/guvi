7b04bb31d6c528d501977e36ecdf1b1fd59317ff
gvs77 <53341630+gvs77@users.noreply.github.com>
2019-08-02 11:20:48 +0530

    Update set1p3.py

--- set1p3.py
+++ set1p3.py
@@ -1,7 +1,8 @@
 c=input()
-if(c=='a' or c=='e' or c=='i' or c=='o' or c=='u'):
-  print("Vowles")
-elif(c!='a' or c!='e' or c!='i' or c!='o' or c!='u'):
-  print("Consonant")
+if(c.isAlpha()==True):
+  if(c=='a' or c=='e' or c=='i' or c=='o' or c=='u'):
+    print("Vowles")
+  elif(c!='a' or c!='e' or c!='i' or c!='o' or c!='u'):
+    print("Consonant")
 else:
   print("Invalid")
