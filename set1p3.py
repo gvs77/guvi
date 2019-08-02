388a1231f28886458644084b1860e568fa64f6cc
gvs77 <53341630+gvs77@users.noreply.github.com>
2019-08-02 10:58:54 +0530

    Update set1p3.py

--- set1p3.py
+++ set1p3.py
@@ -1,5 +1,7 @@
 c=input()
 if(c=='a' or c=='e' or c=='i' or c=='o' or c=='u'):
   print("vowles")
+elif(c!='a' or c!='e' or c!='i' or c!='o' or c!='u'):
+  print("consonant")
 else:
   print("invalid")
